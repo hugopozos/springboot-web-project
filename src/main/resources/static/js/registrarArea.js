5928c4e48c113c3d6e797cfbe2ac965d1ff03455
Hugo <pozos_herrera@outlook.es>
2023-05-30 06:55:49 -0600

    Muestra variables de huerto.html

---
+++ src/main/resources/static/js/registrarArea.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    //cargarHuertos();
+    actualizarEmailDelUsuario();
+});
+
+function actualizarEmailDelUsuario() {
+    document.getElementById('txt-email-usuario').outerHTML = localStorage.email;
+}
+
+function logout() {
+    localStorage.removeItem('token'); // Elimina el token de la sesión
+    window.location.href = 'login.html'; // Redirige al usuario a login.html
+}
